ba0279fcac027904501e0e3a5638a50013a4c441
kpateldev <57636867+kpateldev@users.noreply.github.com>
2021-09-18 18:11:05 -0400

    Logo updates/karen removal

--- features/step_definitions/landing.rb
+++ features/step_definitions/landing.rb
@@ -6,11 +6,11 @@ end
 Then(/^I should find demo script$/) do
   assert page.has_text?("Lost Password?")
   assert page.has_no_text?("Dedicated online photo gallery for each of your clients")
-  assert (page.body).include?("karen@example.com")
+  assert (page.body).include?("info@oneforsafety.io")
 end
 
 Given(/^I click normal login button and enter credentials$/) do
-  User.create!(email: "karen@example.com", password: "password", business_name: "Karen Ling", website: "http://twinkieandkaren.com/", screenshot: 'http://images.karenling.net/aa/user-cover-images/karen_ling.png', activated: true, avatar: 'http://images.karenling.net/aa/karen_ling.jpg')
+  User.create!(email: "info@oneforsafety.io", password: "password", business_name: "One for Safety", website: "http://oneforsafety.io/", screenshot: 'http://images.karenling.net/aa/user-cover-images/karen_ling.png', activated: true, avatar: 'http://images.karenling.net/aa/karen_ling.jpg')
   visit '/'
   first(:link, 'Login').click
 end
@@ -18,8 +18,8 @@ end
 Then(/^I should be logged in$/) do
   assert page.has_text?("Lost Password?")
   assert page.has_no_text?("Dedicated online photo gallery for each of your clients")
-  assert !(page.body).include?("karen@example.com")
-  fill_in 'user-email', :with => 'karen@example.com'
+  assert !(page.body).include?("info@oneforsafety.io")
+  fill_in 'user-email', :with => 'info@oneforsafety.io'
   fill_in 'user-password', :with => 'password'
   page.find(".login-user").click
   assert page.has_content?("Collections")
@@ -33,5 +33,5 @@ end
 Then(/^I should not be logged in$/) do
   assert page.has_no_content?("Dedicated online photo gallery for each of your clients")
   assert page.has_content?("Lost Password?")
-  assert !(page.body).include?("karen@example.com")
+  assert !(page.body).include?("info@oneforsafety.io")
 end
