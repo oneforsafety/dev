d8ba9ae0c8479baea79688ccd4add54cb9b2905b
Paul Seiler <paulseileriv@gmail.com>
2021-09-18 20:21:06 -0400

    Updated email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'oneforsafety.io',
     :enable_starttls_auto => true
   }
 
